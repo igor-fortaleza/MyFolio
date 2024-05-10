a0fd641ba11f6ea2e61fabdbf9c7380d094295a2
igorfortaleza <igorf135@gmail.com>
2024-05-10 03:08:06 -0300

    add .env

--- src/data/colleges.js
+++ src/data/colleges.js
@@ -24,7 +24,6 @@ const myColleges_en = [
 		description:
 			"AZ-900 is a certification that covers the essential concepts of Azure, offering professionals a deep understanding of the capabilities and benefits of Microsoft's cloud platform. This certification is an important step for anyone looking to specialize in Azure, aligning with the growing trend of cloud computing adoption in the market.",
 		link: "https://www.credly.com/badges/62ea4067-eb56-45ff-bc2e-71d144ada45c",
-		logo: "./microsoft_logo.png",
 	},
 	{
 		date: "2018",
@@ -32,7 +31,6 @@ const myColleges_en = [
 		description:
 			"Certified in OpenHack: Containers, this professional has fundamental skills in using Microsoft Azure Kubernetes Service to containerize and deploy applications in the cloud. The certification proves the ability to apply key Azure technologies, especially in preparing clusters for production, excelling in modern cloud solutions.",
 		link: "https://www.credly.com/badges/d7a5dfc5-174d-4e3f-9372-4359ae7f3a6a?source=linked_in_profile",
-		logo: "./microsoft_logo.png",
 	},
 ];
 
